2e62fbaf91f4b6db18f4560929e351b3c277fb79
Nour Sabir <sabir.med.nour@gmail.com>
2026-01-10 12:50:38 -0500

    Tile based movement implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ public class PlayerController : MonoBehaviour
 {
 
     public Rigidbody2D rb;
-    public float speed;
+    public float xStep;
+    public float yStep;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,7 +15,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
-
+        rb.position = new Vector2(
+            rb.position.x + (Input.GetKeyDown(KeyCode.A) ? -1 : (Input.GetKeyDown(KeyCode.D) ? 1 : 0)) * xStep,
+            rb.position.y + (Input.GetKeyDown(KeyCode.W) ? 1 : (Input.GetKeyDown(KeyCode.S) ? -1 : 0)) * yStep
+        );
     }
 }
