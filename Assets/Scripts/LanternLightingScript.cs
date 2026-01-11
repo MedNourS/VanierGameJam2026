4c86f9476ecb4a9140e2fcc3abece2088c706e6d
Nour Sabir <sabir.med.nour@gmail.com>
2026-01-10 23:58:24 -0500

    Partial lighting on lit lantern done

--- Assets/Scripts/LanternLightingScript.cs
+++ Assets/Scripts/LanternLightingScript.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.Tilemaps;
@@ -8,16 +9,34 @@ public class LanternLightingScript : MonoBehaviour
     [SerializeField] private Light2D lanternLightObj;
     [SerializeField] private TileBase litLantern;
 
+    private List<Light2D> lanternLights;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        int lanternCount = 0;
+
+        TileBase[] lanternTiles = lanternsTileMap.GetTilesBlock(lanternsTileMap.cellBounds);
+
+        for (int i = 0; i < lanternTiles.Length; i++)
+        {
+            if (lanternTiles[i] != null)
+            {
+                lanternCount++;
+            }
+        }
+
+        lanternLights = new List<Light2D>();
+
         foreach (var pos in lanternsTileMap.cellBounds.allPositionsWithin)
         {
             Vector3Int tilePos = lanternsTileMap.WorldToCell(pos);
             Debug.Log(pos);
-            if (lanternsTileMap.GetTile(tilePos) == litLantern)
+            if (lanternsTileMap.GetTile(tilePos) != null)
             {
-                Instantiate(lanternLightObj, tilePos, Quaternion.identity);
+                Light2D light = Instantiate(lanternLightObj, tilePos, Quaternion.identity);
+                light.intensity = 0;
+                lanternLights.Add(light);
             }
         }
     }
@@ -25,5 +44,30 @@ public class LanternLightingScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // foreach (var pos in lanternsTileMap.cellBounds.allPositionsWithin)
+        // {
+        //     Vector3Int tilePos = lanternsTileMap.WorldToCell(pos);
+        //     Debug.Log(pos);
+        //     if (lanternsTileMap.GetTile(tilePos) == litLantern)
+        //     {
+        //         Light2D light = Instantiate(lanternLightObj, tilePos, Quaternion.identity);
+        //         light.intensity = 0;
+        //         lanternLights.Add(light);
+        //     }
+        // }
+
+        TileBase[] lanternTiles = lanternsTileMap.GetTilesBlock(lanternsTileMap.cellBounds);
+
+        for (int i = 0; i < lanternTiles.Length; i++)
+        {
+            if (lanternTiles[i] == litLantern)
+            {
+                lanternLights[i].intensity = 1;
+            }
+            else
+            {
+                lanternLights[i].intensity = 0;
+            }
+        }
     }
 }
